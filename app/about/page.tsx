7afc9f19e786ab7e0a6904cdd860c55f04848025
Syed Munir Ullah <syedmunirullah01@gmail.com>
2024-12-08 15:11:16 +0500

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import AboutHero from '../components/aboutHero'
 import AboutSection from '../components/aboutSection'
-import Footer from '../components/Footer';
 import AboutPopularProduct from '../components/aboutPopularProduct';
 
 const page = () => {
@@ -10,7 +9,7 @@ const page = () => {
         <AboutHero />
             <AboutSection />
             <AboutPopularProduct />
-            <Footer />
+            
       </div>
     );
 }
